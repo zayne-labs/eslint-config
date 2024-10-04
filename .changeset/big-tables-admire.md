75e4353566d9d5c8adaf9f81d411071816fd9328
Ryan Zayne <phoenixastro97@gmail.com>
2024-10-04 16:32:42 +0100

    chore: disable ts-eslint no-use-before-define rule

---
+++ .changeset/big-tables-admire.md
@@ -0,0 +1,5 @@
+---
+"@zayne-labs/eslint-config": patch
+---
+
+turn off ts rule no use before define
--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -126,7 +126,7 @@ export const typescript = async (
 					},
 				],
 				"ts-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", ignoreRestSiblings: true }],
-				"ts-eslint/no-use-before-define": ["error", { functions: false, ignoreTypeReferences: true }],
+				"ts-eslint/no-use-before-define": "off",
 				"ts-eslint/no-useless-constructor": "error",
 
 				...(isTypeAware && typeAwareRules),
