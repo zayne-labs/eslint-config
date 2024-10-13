dcf597147b8ac80fa7c73e3b10d17cbc762e50bc
ryan-zayne <phoenixastro97@gmail.com>
2024-10-13 01:05:10 +0100

    fix(tanstack): fix another bug in tanstack config

---
+++ .changeset/short-worms-swim.md
@@ -0,0 +1,5 @@
+---
+"@zayne-labs/eslint-config": patch
+---
+
+fix: another bug in tanstack
--- src/configs/tanstack.ts
+++ src/configs/tanstack.ts
@@ -7,27 +7,33 @@ const tanstack = async (
 ): Promise<TypedFlatConfigItem[]> => {
 	const { query = true } = options;
 
-	await ensurePackages(["@tanstack/eslint-plugin-query"]);
-
-	const eslintPluginTanstackQuery = await interopDefault(import("@tanstack/eslint-plugin-query"));
-
-	return [
+	const tanstackConfig: TypedFlatConfigItem[] = [
 		{
 			name: "zayne/tanstack/setup",
 			plugins: {
-				...(query && { "tanstack/query": eslintPluginTanstackQuery }),
+				...(query && {
+					"tanstack/query": await interopDefault(import("@tanstack/eslint-plugin-query")),
+				}),
 			},
 		},
-
-		{
-			name: "zayne/tanstack/query-recommended",
-			...(query &&
-				renameRules(
-					eslintPluginTanstackQuery.configs["flat/recommended"][0]?.rules,
-					defaultPluginRenameMap
-				)),
-		},
 	];
+
+	if (query) {
+		await ensurePackages(["@tanstack/eslint-plugin-query"]);
+
+		const eslintPluginTanstackQuery = await interopDefault(import("@tanstack/eslint-plugin-query"));
+
+		tanstackConfig.push({
+			name: "zayne/tanstack/query-recommended",
+
+			rules: renameRules(
+				eslintPluginTanstackQuery.configs["flat/recommended"][0]?.rules,
+				defaultPluginRenameMap
+			),
+		});
+	}
+
+	return tanstackConfig;
 };
 
 export { tanstack };
